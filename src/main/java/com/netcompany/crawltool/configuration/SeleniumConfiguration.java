65e35ed9c36e299ba157059372546b708abe6db5
Nhat180 <s3740975@rmit.edu.vn>
2022-06-11 21:09:59 +0700

    Update SeleniumConfiguration.java

--- src/main/java/com/netcompany/crawltool/configuration/SeleniumConfiguration.java
+++ src/main/java/com/netcompany/crawltool/configuration/SeleniumConfiguration.java
@@ -22,7 +22,7 @@ public class SeleniumConfiguration {
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        options.setBinary(" /app/.apt/usr/bin/google-chrome");
+        options.setBinary("/app/.apt/usr/bin/google-chrome");
         return new ChromeDriver(options);
     }
 }
